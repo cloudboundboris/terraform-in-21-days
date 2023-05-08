3ecb3de57adefb3391af207254f0dea1f4909a93
Boris Levenzon <blevenzon@gmail.com>
2023-05-08 10:37:48 -0400

    Deploy EC2 using Datasources (#6)

--- instance.tf
+++ instance.tf
@@ -1,14 +1,31 @@
 locals {
   mypublicip = sensitive(file("~/.ssh/mypubip.txt"))
 }
+
+data "aws_ami" "amazonlinux" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-kernel-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+}
+
+
 resource "aws_instance" "public" {
 
-  ami                         = "ami-0889a44b331db0194"
+  ami                         = data.aws_ami.amazonlinux.id
   associate_public_ip_address = true
   instance_type               = "t2.micro"
   key_name                    = "mainkeypair"
   vpc_security_group_ids      = [aws_security_group.public.id]
-  subnet_id                   = aws_subnet.public[0].id 
+  subnet_id                   = aws_subnet.public[0].id
 
   tags = {
     Name = "${var.env_code}-public"
@@ -17,65 +34,65 @@ resource "aws_instance" "public" {
 
 resource "aws_instance" "private" {
 
-  ami                         = "ami-0889a44b331db0194"
-  instance_type               = "t2.micro"
-  key_name                    = "mainkeypair"
-  vpc_security_group_ids      = [aws_security_group.private.id]
-  subnet_id                   = aws_subnet.private[0].id 
+  ami                    = data.aws_ami.amazonlinux.id
+  instance_type          = "t2.micro"
+  key_name               = "mainkeypair"
+  vpc_security_group_ids = [aws_security_group.private.id]
+  subnet_id              = aws_subnet.private[0].id
 
   tags = {
     Name = "${var.env_code}-private"
   }
 }
 
-  resource "aws_security_group" "public" {
-    name        = "${var.env_code}-publicSG"
-    description = "Allow SSH traffic"
-    vpc_id      = aws_vpc.main.id
+resource "aws_security_group" "public" {
+  name        = "${var.env_code}-publicSG"
+  description = "Allow SSH traffic"
+  vpc_id      = aws_vpc.main.id
 
-    ingress {
-      description = "SSH from Public"
-      from_port   = 22
-      to_port     = 22
-      protocol    = "tcp"
-      cidr_blocks = [local.mypublicip]
-    }
-
-    egress {
-      from_port   = 0
-      to_port     = 0
-      protocol    = "-1"
-      cidr_blocks = ["0.0.0.0/0"]
-
-    }
-
-    tags = {
-      Name = "${var.env_code}-publicSG"
-    }
+  ingress {
+    description = "SSH from Public"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = [local.mypublicip]
   }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+
+  }
+
+  tags = {
+    Name = "${var.env_code}-publicSG"
+  }
+}
 
 resource "aws_security_group" "private" {
-    name        = "${var.env_code}-privateSG"
-    description = "Allow SSH from VPC"
-    vpc_id      = aws_vpc.main.id
+  name        = "${var.env_code}-privateSG"
+  description = "Allow SSH from VPC"
+  vpc_id      = aws_vpc.main.id
 
-    ingress {
-      description = "SSH from VPC"
-      from_port   = 22
-      to_port     = 22
-      protocol    = "tcp"
-      cidr_blocks = [var.vpc_cidr]
-    }
-
-    egress {
-      from_port   = 0
-      to_port     = 0
-      protocol    = "-1"
-      cidr_blocks = ["0.0.0.0/0"]
-
-    }
-
-    tags = {
-      Name = "${var.env_code}-privateSG"
-    }
+  ingress {
+    description = "SSH from VPC"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = [var.vpc_cidr]
   }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+
+  }
+
+  tags = {
+    Name = "${var.env_code}-privateSG"
+  }
+}
--- vpc.tf
+++ vpc.tf
@@ -11,24 +11,24 @@ resource "aws_vpc" "main" {
 }
 
 resource "aws_subnet" "public" {
-  count = length(var.public_cidr)
-  vpc_id     = aws_vpc.main.id
-  cidr_block = var.public_cidr[count.index]
+  count             = length(var.public_cidr)
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = var.public_cidr[count.index]
   availability_zone = local.availability_zones[count.index]
 
   tags = {
-    Name = "${var.env_code}-public${count.index+1}"
+    Name = "${var.env_code}-public${count.index + 1}"
   }
 }
 
 resource "aws_subnet" "private" {
-  count = length(var.private_cidr)
-  vpc_id     = aws_vpc.main.id
-  cidr_block = var.private_cidr[count.index]
+  count             = length(var.private_cidr)
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = var.private_cidr[count.index]
   availability_zone = local.availability_zones[count.index]
 
   tags = {
-    Name = "${var.env_code}-private${count.index+1}"
+    Name = "${var.env_code}-private${count.index + 1}"
   }
 }
 
@@ -54,42 +54,42 @@ resource "aws_route_table" "public" {
 }
 
 resource "aws_route_table_association" "public" {
-  count = length(var.public_cidr)
+  count          = length(var.public_cidr)
   subnet_id      = aws_subnet.public[count.index].id
   route_table_id = aws_route_table.public.id
 }
 
 resource "aws_eip" "nat" {
   count = length(var.public_cidr)
-  vpc      = true
+  vpc   = true
 
   tags = {
-    Name = "${var.env_code}-nat${count.index+1}"
+    Name = "${var.env_code}-nat${count.index + 1}"
   }
 }
 
 resource "aws_nat_gateway" "main" {
-  count = length(var.public_cidr)
+  count         = length(var.public_cidr)
   allocation_id = aws_eip.nat[count.index].id
   subnet_id     = aws_subnet.public[count.index].id
 
   tags = {
-    Name = "${var.env_code}-main_nat_gw${count.index+1}"
-  }    
+    Name = "${var.env_code}-main_nat_gw${count.index + 1}"
+  }
   depends_on = [aws_internet_gateway.main]
 }
 
 resource "aws_route_table" "private" {
-  count = length(var.public_cidr)
+  count  = length(var.public_cidr)
   vpc_id = aws_vpc.main.id
 
   route {
-    cidr_block = "0.0.0.0/0"
+    cidr_block     = "0.0.0.0/0"
     nat_gateway_id = aws_nat_gateway.main[count.index].id
   }
 
   tags = {
-    Name = "${var.env_code}-private_route_table${count.index+1}"
+    Name = "${var.env_code}-private_route_table${count.index + 1}"
   }
 }
 
