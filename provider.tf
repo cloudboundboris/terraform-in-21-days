b1115e1719d0d1a65944a4a2d5fe9ef7e136eb93
Boris Levenzon <blevenzon@gmail.com>
2023-05-10 07:26:21 -0400

    Config terraform remote-state

--- provider.tf
+++ provider.tf
@@ -1,3 +1,14 @@
+terraform {
+  backend "s3" {
+    bucket = "terraform-remote-state-bl"
+    key    = "terraform.tfstate"
+    region = "us-east-1"
+    dynamodb_table = "terraform-remote-state-db"
+  }
+}
+
+
+
 provider "aws" {
   region = "us-east-1"
 }
