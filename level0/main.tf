94ad881c8d8277ba5b52fccb254074fb7fd273ed
Boris Levenzon <blevenzon@gmail.com>
2023-05-14 07:03:42 -0400

    forgot to add level0

---
+++ level0/main.tf
@@ -0,0 +1,19 @@
+resource "aws_s3_bucket" "remote_state" {
+  bucket = "terraform-remote-state-bl"
+}
+
+resource "aws_dynamodb_table" "terraform-remote-state-db" {
+  name           = "terraform-remote-state-db"
+  billing_mode   = "PROVISIONED"
+  read_capacity  = 1
+  write_capacity = 1
+  hash_key       = "LockID"
+
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+
+}
+
